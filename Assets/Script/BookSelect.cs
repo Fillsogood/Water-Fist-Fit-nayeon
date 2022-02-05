52d832eac7789290ed59931c495ff7e9457f5475
Fillsogood <94848819+Fillsogood@users.noreply.github.com>
2022-02-05 16:21:10 +0900

    d

---
+++ Assets/Script/BookSelect.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookSelect : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+   private void Unity_BookSelect()
+   {
+       
+   }
+}
